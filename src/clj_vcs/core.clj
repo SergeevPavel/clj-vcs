939dd8dcd410dbe5f8aeeb64a7ef2dd1cb8d9ec0
Pavel Sergeev <sergeev.pavel.al@gmail.com>
2016-06-18 11:05:10 +0300

    commit and history implemented

--- src/clj_vcs/core.clj
+++ src/clj_vcs/core.clj
@@ -5,3 +5,69 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+
+(def initial-repo-state {:index {
+                                 0 [{} []]
+                                 }
+                         :branches {"master" 0}
+                         :current "master"
+                         })
+
+(defn get-next-commit-id
+  [repo]
+  (let [ks (keys (:index repo))
+        m (apply max ks)]
+    (inc m)))
+
+(defn get-current-id
+  [repo]
+  (let [current (:current repo)]
+    ((:branches repo) current)))
+
+(defn commit
+  "Add new snapshot to current branch."
+  [repo snapshot]
+  (let [id (get-next-commit-id repo)
+        parent-id (get-current-id repo)
+        {i :index b :branches c :current} repo]
+    {:index (assoc i id [snapshot [parent-id]])
+     :branches (assoc b c id)
+     :current c
+     }))
+
+(defn get-ids-history
+  [repo]
+  (let [{index :index} repo]
+    (loop [id (get-current-id repo) history []]
+      (if (nil? id)
+        history
+        (recur (get-in index [id 1 0])
+               (conj history id))))))
+
+(defn history
+  "Return history of changes."
+  [repo path]
+  (let [ids-history (get-ids-history repo)
+        snapshots (map (fn [id] (get-in repo [:index id 0])) ids-history)]
+    (map (fn [sn] (get-in sn path)) snapshots)))
+
+(defn branch
+  "Create new branch."
+  [repo branch-name]
+  nil)
+
+(defn checkout
+  "Switch current branch."
+  [repo branch-name]
+  nil)
+
+(defn merge-branches
+  "Merge current branch to target."
+  [repo target]
+  nil)
+
+(defn rebase
+  "Reabase commits from current to target."
+  [repo target]
+  nil)
--- test/clj_vcs/core_test.clj
+++ test/clj_vcs/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [clj-vcs.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def r1 (commit initial-repo-state {:a 1 :b 2}))
+
+(def r2 (commit r1 {:a 2 :c {:x [1 2 3]}}))
+
+(deftest get-ids-history-test
+  (testing "Linear history"
+    (is (= [0]) (get-ids-history initial-repo-state))
+    (is (= [1 0]) (get-ids-history r1))
+    (is (= [2 1 0] (get-ids-history r2)))))
+
+(deftest history-test
+  (testing "History of changes by path"
+    (is (= [nil] (history initial-repo-state [:a])))
+    (is (= [1 nil] (history r1 [:a])))
+    (is (= [2 1 nil] (history r2 [:a])))
+    (is (= [3 nil nil] (history r2 [:c :x 2])))))
